ed88c0885dbec09530bc9429f6895a5c176a45d3
Josiah Siegel <josiah.siegel@semanticbits.com>
2023-05-16 12:15:36 -0400

    refactor

--- src/utils/github/default_branch.js
+++ src/utils/github/default_branch.js
@@ -3,10 +3,9 @@ import request from "./request.js";
 const default_branch = async function ({ api, app, token, say, msg }) {
   const path = `/repos/${app}`;
   const method = "GET";
-  const data = "";
+  const data = null;
   const out = await request({ api, path, method, token, data, say, msg });
-  const json = JSON.parse(out);
-  return json.default_branch;
+  return JSON.parse(out)?.default_branch;
 };
 
 export default default_branch;
